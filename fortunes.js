b1b1138c9818e76459b55ab57432d6bf418c10b2
Brendan Clune <brendan@bclune.org>
2013-07-30 22:25:46 -0400

    Escape html in commit messages, cache failed Github responses

--- fortunes.js
+++ fortunes.js
@@ -1,13 +1,14 @@
-var redis = require('redis'),
+var shuffle = require('shuffle'),
+    request = require('request'),
+    redis = require('redis'),
     redisClient = redis.createClient();
 
-var request = require('request');
 var view = require('./view');
 
-var shuffle = require('shuffle');
 
 // redis cache expiration in seconds
-var CACHE_TIMEOUT = 300;
+var CACHE_TIMEOUT = 10;
+var REDIS_404_TOKEN = "_404";
 
 function fetchCommitHistoryFromGithub(repository, serverResponse) {
     apiString = 'https://api.github.com/repos' + repository + '/commits';
@@ -17,7 +18,8 @@ function fetchCommitHistoryFromGithub(repository, serverResponse) {
             extractMessage(body, serverResponse);
             redisClient.setex(repository, CACHE_TIMEOUT, body);
         } else {
-            view.writeNotFound();
+            view.writeNotFound(serverResponse);
+            redisClient.setex(repository, CACHE_TIMEOUT, REDIS_404_TOKEN);
         }
     });
 }
@@ -30,9 +32,12 @@ function extractMessage(commitJson, response) {
 
 function getGithubResponseFromCache(repository, response) {
     redisClient.get(repository, function(err, res) { 
+        console.log('Got from redis: ' + res);
         if (res === null) {
             console.log('Refreshing cache from github repo: ' + repository);
             fetchCommitHistoryFromGithub(repository, response);
+        } else if (res === REDIS_404_TOKEN) {
+            view.writeNotFound(response);
         } else {
             extractMessage(res, response);
         }
--- view.js
+++ view.js
@@ -1,5 +1,6 @@
+var escape = require('escape-html');
 function writeMessage(messageText, response) {
-    var message = '<html><body><pre>' + messageText + '</pre></body><html>';
+    var message = '<html><body><pre>' + escape(messageText) + '</pre></body><html>';
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(message);
